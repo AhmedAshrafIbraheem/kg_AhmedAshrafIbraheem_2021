1029f169356bcd80009188af18d93567d5b3d716
Ahmed Moussa <ahmed.a.moussa2017@gmail.com>
2020-03-20 02:10:36 -0400

    Program Functionality is finished

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ import sys
 
 
 def can_map_strings(s1: str, s2: str) -> bool:
+    if len(s1) > len(s2):
+        return False
+
+    mapper: Dict[str, str] = {}
+    for i in range(len(s1)):
+        if s1[i] not in mapper:
+            mapper[s1[i]] = s2[i]
+        else:
+            if mapper[s1[i]] != s2[i]:
+                return False
+
     return True
 
 
