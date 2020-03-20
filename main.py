9c87a0d4fe3b638a7ad883687366da4d2ce0f2c9
Ahmed Moussa <ahmed.a.moussa2017@gmail.com>
2020-03-20 02:11:12 -0400

    Comments added

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 import sys
+from typing import Dict
 
 
 def can_map_strings(s1: str, s2: str) -> bool:
     if len(s1) > len(s2):
+    	# length of first string is longer
+    	# than that of the second one so there
+    	# will be no possible mapping between them
         return False
 
+    # mapper store every character with its mapped value 
     mapper: Dict[str, str] = {}
+
     for i in range(len(s1)):
         if s1[i] not in mapper:
+            # if the character is new
+            # add it to our mapper
             mapper[s1[i]] = s2[i]
         else:
             if mapper[s1[i]] != s2[i]:
+                # if the character is already in our mapper
+        	# and it has a value that is different from
+        	# the current character, that means there
+    		# will be no possible mapping
                 return False
-
     return True
 
 
